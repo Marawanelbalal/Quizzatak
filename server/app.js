fe1ed5d06a784bedc5064a8cfcf03aee06767a27
Marawanelbalal <marawananbu100@gmail.com>
2026-04-01 14:13:41 +0200

    Returning CORS

--- server/app.js
+++ server/app.js
@@ -5,18 +5,12 @@ import classroomRoutes from "./routes/classroomRoutes.js";
 import quizRoutes from "./routes/quizRoutes.js";
 import gameSessionRoutes from "./routes/gameSessionRoutes.js";
 import submissionRoutes from "./routes/submissionRoutes.js";
+import { createCorsOptions } from "./config/index.js";
 import { notFoundHandler, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://quizzatak.netlify.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
-});
-app.use(cors({ origin: "*", credentials: false }));
+
+app.use(cors(createCorsOptions()));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
