4bf7d89232af288a1b7c6b43d98ddd0d0a8a8fdb
Marawanelbalal <marawananbu100@gmail.com>
2026-04-01 14:02:09 +0200

    Bypassing CORS

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ import classroomRoutes from "./routes/classroomRoutes.js";
 import quizRoutes from "./routes/quizRoutes.js";
 import gameSessionRoutes from "./routes/gameSessionRoutes.js";
 import submissionRoutes from "./routes/submissionRoutes.js";
-import { createCorsOptions } from "./config/index.js";
 import { notFoundHandler, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
@@ -17,7 +16,7 @@ app.use((req, res, next) => {
   if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
 });
-app.use(cors(createCorsOptions()));
+app.use(cors({ origin: "*", credentials: false }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
